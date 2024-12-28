46bab8ce69e68c82052811b4cc6ae46363247502
Kamil Cukrowski <kamilcukrowski@gmail.com>
2024-12-28 14:02:35 +0100

    argparse: add subparsers, parsing { }, and func:* actions for subparsers

---
+++ scripts/argparse_func.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+CMD_a() {
+	L_argparse description='a help' -- bar type=int help='bar help' ---- "$@"
+}
+CMD_b() {
+	L_argparse prog=helpme description='b help' epilog='suffix' -- --baz choices='X Y Z' help='baz help' ---- "$@"
+}
+export -f CMD_a CMD_b
+args=(
+	-- --foo action=store_true help='foo help' \
+	-- cmd action=func:CMD_ \
+	---- "$@"
+)
+echo "+ L_argparse ${args[*]@Q}"
+"$(dirname "$0")"/L_argparse "${args[@]}"
