f626e89ab7d0ff9be7e60cf091e7903e00dffe05
Kamil Cukrowski <kamilcukrowski@gmail.com>
2025-02-15 00:10:30 +0100

    argparse: work on completion

--- scripts/env.sh
+++ scripts/env.sh
@@ -1,5 +1,6 @@
 export PATH=$PATH:$PWD
 sh=$(ps | tail -n 4 | sed -E '2,$d;s/.* (.*)/\1/')
 echo "loading $sh"
-eval "$(completetest2.sh "--L_argparse_complete_$sh" | tee /dev/stderr)"
-eval "$(completetest.sh "--L_argparse_complete_$sh" | tee /dev/stderr)"
+for i in ./complete*.sh; do
+	eval "$( "${i##./}" "--L_argparse_complete_$sh" | tee /dev/stderr )"
+done
