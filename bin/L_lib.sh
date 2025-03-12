1e4dfafe71038727d2549f25af8f997a8ea8843a
unknown <kamilcukrowski@gmail.com>
2025-03-12 10:05:48 +0100

    bump version

--- bin/L_lib.sh
+++ bin/L_lib.sh
@@ -24,7 +24,7 @@
 #@description some global variables
 
 # @description version of the library
-L_LIB_VERSION=0.1.7
+L_LIB_VERSION=0.1.8
 # @description The basename part of $0
 L_NAME=${0##*/}
 if [[ "$0" == */* ]]; then
