6a54f90eec0051c3f3da916b603fe4e9ea77b246
Kamil Cukrowski <kamilcukrowski@gmail.com>
2025-07-28 23:21:51 +0200

    add L_uuid4 function

--- bin/L_lib.sh
+++ bin/L_lib.sh
@@ -1116,6 +1116,32 @@ L_bashpid_to() {
 	printf -v "$1" "%s" "${BASHPID:-$(exec "${BASH:-sh}" -c 'echo "$PPID"')}"
 }
 
+# @description Generate uuid in bash.
+# @option -v <var> var
+# @see https://digitalbunker.dev/understanding-how-uuids-are-generated/
+L_uuid4() { L_handle_v_scalar "$@"; }
+L_uuid4_v() {
+	# Generate 128 random bits
+	# RANDOM has 16 bits. 128/16=8
+	# SRANDOM has 32 bits. 128/32=4
+	# Take the 7th byte and perform an AND operation with 0x0F to clear out the high nibble.
+	# Then, OR it with 0x40 to set the version number to 4.
+	# Next, take the 9th byte and perform an AND operation with 0x3F and then OR it with 0x80.
+	# Convert the 128 bits to hexadecimal representation and insert the hyphens to achieve the canonical text representation.
+	if ((L_HAS_SRANDOM)); then
+		printf -v L_v "%08x%08x%08x%08x" \
+			"$SRANDOM" "$(( SRANDOM & 0x3FFFFFFF | 0x40000000 ))" "$SRANDOM" "$SRANDOM"
+	elif L_hash uuidgen; then
+		L_v=$(uuidgen)
+		return
+	else
+		printf -v L_v "%04x%04x%04x%04x""%04x%04x%04x%04x" \
+			"$RANDOM" "$RANDOM" "$(( RANDOM & 0x3FFF | 0x4000 ))" "$RANDOM" \
+			"$RANDOM" "$RANDOM" "$RANDOM" "$RANDOM"
+	fi
+	L_v=${L_v::8}-${L_v:8:4}-4${L_v:13:3}-${L_v:16:4}-${L_v:20}
+}
+
 # ]]]
 # exit_to [[[
 # @section exit_to
@@ -1500,6 +1526,7 @@ L_LBRACE='{'
 # @description Right brace character
 L_RBRACE='}'
 # @description Looks random.
+# @see L_uuid4
 L_UUID=921c7f46-e0d8-4170-91e9-7055ee30d1e2
 # @description 255 bytes with all possible 255 values
 L_ALLCHARS=$'\001\002\003\004\005\006\007\010\011\012\013\014\015\016\017\020\021\022\023\024\025\026\027\030\031\032\033\034\035\036\037\040\041\042\043\044\045\046\047\050\051\052\053\054\055\056\057\060\061\062\063\064\065\066\067\070\071\072\073\074\075\076\077\100\101\102\103\104\105\106\107\110\111\112\113\114\115\116\117\120\121\122\123\124\125\126\127\130\131\132\133\134\135\136\137\140\141\142\143\144\145\146\147\150\151\152\153\154\155\156\157\160\161\162\163\164\165\166\167\170\171\172\173\174\175\176\177\200\201\202\203\204\205\206\207\210\211\212\213\214\215\216\217\220\221\222\223\224\225\226\227\230\231\232\233\234\235\236\237\240\241\242\243\244\245\246\247\250\251\252\253\254\255\256\257\260\261\262\263\264\265\266\267\270\271\272\273\274\275\276\277\300\301\302\303\304\305\306\307\310\311\312\313\314\315\316\317\320\321\322\323\324\325\326\327\330\331\332\333\334\335\336\337\340\341\342\343\344\345\346\347\350\351\352\353\354\355\356\357\360\361\362\363\364\365\366\367\370\371\372\373\374\375\376\377'
