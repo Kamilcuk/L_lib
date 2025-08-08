102eabbcda38539bd28f16bcb48ead533b93f524
Kamil Cukrowski <kamilcukrowski@gmail.com>
2025-08-08 12:11:14 +0200

    fix testing json escape also with curl

--- tests/test.sh
+++ tests/test.sh
@@ -388,6 +388,55 @@ _L_test_format() {
 	}
 }
 
+_L_test_json_escape() {
+	{
+		if L_hash jq; then
+			t() {
+				local tmp
+				L_json_escape -v tmp "$1"
+				# L_log "JSON ${tmp@Q}"
+				out=$(echo "{\"v\":$tmp}" | jq -r .v)
+				L_unittest_eq "$1" "$out"
+			}
+			t $'1 hello\n\t\bworld'
+			t $'2 \x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f'
+			t $'3 \x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1a\x1b\x1c\x1d\x1e\x1f'
+			t $'4 \x20\x21\x22\x23\x24\x25\x26\x27\x28\x29\x2a\x2b\x2c\x2d\x2e\x2f'
+			t $'5 \x40\x41\x42\x43\x44\x45\x46\x47\x48\x49\x4a\x4b\x4c\x4d\x4e\x4f'
+			t "! ${L_ALLCHARS::127}"
+			unset -f t
+		fi
+	}
+	{
+		if L_hash curl; then
+			t() {
+				local tmp
+				L_json_escape -v tmp "$1"
+				# L_log "JSON ${tmp@Q}"
+				L_unittest_cmd -o '{"errors":[],"valid":true}' curl -sS \
+					-H "Content-Type: application/json" \
+					-d'{
+  						"schema": {
+  							"type": "object",
+  							"properties": { "v": { "type": "string" }},
+  							"required": ["v"]
+							},
+  						"json": {
+  							"v": '"$tmp"'
+  						}
+						}' "https://assertible.com/json"
+			}
+			t $'1 hello\n\t\bworld'
+			t $'2 \x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f'
+			t $'3 \x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1a\x1b\x1c\x1d\x1e\x1f'
+			t $'4 \x20\x21\x22\x23\x24\x25\x26\x27\x28\x29\x2a\x2b\x2c\x2d\x2e\x2f'
+			t $'5 \x40\x41\x42\x43\x44\x45\x46\x47\x48\x49\x4a\x4b\x4c\x4d\x4e\x4f'
+			t "! ${L_ALLCHARS::127}"
+			unset -f t
+		fi
+	}
+}
+
 _L_test_other() {
 	{
 		local max=-1
@@ -443,24 +492,6 @@ _L_test_other() {
 		L_unittest_checkexit 0 L_args_contain 1 1
 		L_unittest_checkexit 1 L_args_contain 0 1
 		L_unittest_checkexit 1 L_args_contain 0
-	}
-	{
-		if L_hash jq; then
-			local tmp
-			t() {
-				local tmp
-				L_json_escape -v tmp "$1"
-				# L_log "JSON ${tmp@Q}"
-				out=$(echo "{\"v\":$tmp}" | jq -r .v)
-				L_unittest_eq "$1" "$out"
-			}
-			t $'1 hello\n\t\bworld'
-			t $'2 \x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f'
-			t $'3 \x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1a\x1b\x1c\x1d\x1e\x1f'
-			t $'4 \x20\x21\x22\x23\x24\x25\x26\x27\x28\x29\x2a\x2b\x2c\x2d\x2e\x2f'
-			t $'5 \x40\x41\x42\x43\x44\x45\x46\x47\x48\x49\x4a\x4b\x4c\x4d\x4e\x4f'
-			t "! ${L_ALLCHARS::127}"
-		fi
 	}
 	{
 		local name=1 tmp
