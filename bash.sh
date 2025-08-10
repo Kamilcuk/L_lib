d78c4f36c0e2c936332baadb61cd3250a8bb4765
Kamil Cukrowski <kamilcukrowski@gmail.com>
2025-08-10 21:19:28 +0200

    bash.sh: better arguments

--- bash.sh
+++ bash.sh
@@ -1,23 +1,36 @@
 #!/bin/bash
 set -euo pipefail
+. "$(dirname "$0")"/bin/L_lib.sh L_argparse \
+  description="Compare behavior of multiple bash versions" \
+  -- -q --quiet flag=1 help="Silence output from dockers" \
+  -- -i --input help="Stdin" default="" \
+  -- versions nargs="?" help="Bash version to test against, or all. Default: all" default="all" \
+  -- args nargs="*" help="Bash arguments" \
+  ---- "$@"
 
-if [[ "$1" == "all" ]]; then
-  set -- '3.1 3.2 4.0 4.1 4.2 4.3 4.4 5.0 5.1 5.2 5.3-rc2' "${@:2}"
+if [[ "$versions" == "all" ]]; then
+  versions='3.1 3.2 4.0 4.1 4.2 4.3 4.4 5.0 5.1 5.2 5.3 latest'
 fi
 
-input=""
-if read -t 0; then
+if [[ -z "$input" ]] && read -t 0; then
   input=$(cat)
   echo "input=$input"
 fi
 
-IFS=$', \t\n' read -r -a versions <<<"$1"
+IFS=$', \t\n' read -r -a versions <<<"$versions"
 for version in "${versions[@]}"; do
   (
     (
       echo "$version / bash:$version"
       rc=0
-      docker run -i --rm -v $PWD:$PWD:ro bash:"$version" bash "${@:2}" <<<"$input" 2>&1 || rc=$?
+      (
+        if ((quiet)); then
+          exec 1>/dev/null
+        else
+          exec 2>&1
+        fi
+        docker run -q -i --rm -v $PWD:$PWD:ro bash:"$version" bash "${args[@]}" <<<"$input"
+      ) || rc=$?
       echo "\ rc=$rc"
     ) | tr '\n' $'\035'
     echo
