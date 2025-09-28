972b7fff70f6a7ed0399db191e4f74f01afe790b
Kamil Cukrowski <kamilcukrowski@gmail.com>
2025-09-29 00:49:57 +0200

    small changes

--- .nvim.lua
+++ .nvim.lua
@@ -1,6 +1,24 @@
+
 local ok, mason_lspconfig = pcall(require, "mason-lspconfig")
 if ok then
-  mason_lspconfig.setup({
-    automatic_enable = { exclude = { "shellcheck" } },
-  })
+	mason_lspconfig.setup({
+		automatic_enable = { exclude = { "shellcheck" } },
+	})
 end
+
+vim.cmd.LspStop()
+vim.api.nvim_create_autocmd({ "BufEnter", "BufRead" }, {
+	pattern = { "*.sh" },
+	callback = function()
+		pcall(vim.cmd.LspStop)
+	end,
+})
+
+require("nvim-treesitter.configs").setup({
+	highlight = {
+		enable = false, -- false will disable the whole extension
+		disable = { "sh", "bash" }, -- list of language that will be disabled
+	},
+})
+
+
