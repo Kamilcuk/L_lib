baf908e0e5465cbf58138c0981b0c5ed1dd2ffc1
Kamil Cukrowski <kamilcukrowski@gmail.com>
2025-10-09 22:40:01 +0200

    fix L_proc example

--- scripts/proc_example.sh
+++ scripts/proc_example.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-. L_lib.sh
+. $(dirname "$0")/../L_lib.sh
+L_finally wait
+L_finally L_kill_all_childs
 childs=()
 for script in 'sleep 1 && exit 1' 'sleep 2; exit 2' 'sleep 3; exit 3'; do
   L_proc_popen tmp bash -c "$script"
-  childs+=("$(L_array_to_string tmp)")
+  childs+=("$tmp")
 done
 for i in "${childs[@]}"; do
-  L_array_from_string child "$i"
-  declare -p child
-  L_proc_wait child
-  echo "Child [$(L_proc_get_cmd child)] pid $(L_proc_get_pid child) exited with $(L_proc_get_exitcode child)"
+  L_proc_wait i
+  echo "Process [$(L_proc_get_cmd i)] pid $(L_proc_get_pid i) exited with $(L_proc_get_exitcode i)"
 done
