53089b0be06c116df61f8cdd7f837d6fb255d8ab
Kamil Cukrowski <kamilcukrowski@gmail.com>
2026-06-23 00:05:28 +0200

    feat(builtin): add lua, expand, and finalize error handling

--- builtin/L_builtin.h
+++ builtin/L_builtin.h
@@ -5,16 +5,28 @@
 
 int lseek_subcommand(WORD_LIST *list);
 int poll_subcommand(WORD_LIST *list);
+#ifdef HAVE_PPOLL
 int ppoll_subcommand(WORD_LIST *list);
+#endif
 int sigmask_subcommand(WORD_LIST *list);
 int sigunmask_subcommand(WORD_LIST *list);
 int pipe_subcommand(WORD_LIST *list);
 
+#ifdef HAVE_LUA
+int lua_subcommand(WORD_LIST *list);
+#endif
+
 extern char *lseek_doc[];
 extern char *poll_doc[];
+#ifdef HAVE_PPOLL
 extern char *ppoll_doc[];
+#endif
 extern char *sigmask_doc[];
 extern char *sigunmask_doc[];
 extern char *pipe_doc[];
+
+#ifdef HAVE_LUA
+extern char *lua_doc[];
+#endif
 
 #endif
