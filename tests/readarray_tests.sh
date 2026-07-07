da499f0c6356d20b8f3f8db408fbaa905453dcda
Kamil Cukrowski <kamilcukrowski@gmail.com>
2026-07-07 23:54:29 +0200

    L_readarray: fixes, speed up in old bash

--- tests/readarray_tests.sh
+++ tests/readarray_tests.sh
@@ -1,5 +1,5 @@
 _L_test_readarray_1() {
-	local L_HAS_MAPFILE_D=0 L_HAS_MAPFILE=0
+	local L_HAS_MAPFILE=0
 	_L_test_readarray_2
 }
 
@@ -78,3 +78,76 @@ _L_test_readarray_2() {
 	}
 }
 
+_L_test_readarray_tail_0() {
+	_L_printer() { printf "%s\n" {1..25}; }
+	{
+		# 1. without -t, without -n
+		local arr=()
+		L_readarray -s 21 arr < <(_L_printer)
+		L_unittest_arreq arr $'22\n' $'23\n' $'24\n' $'25\n'
+	}
+	{
+		# 2. with -t, without -n
+		local arr=()
+		L_readarray -t -s 21 arr < <(_L_printer)
+		L_unittest_arreq arr 22 23 24 25
+	}
+	{
+		# 3. without -t, with -n
+		local arr=()
+		L_readarray -s 21 -n 2 arr < <(_L_printer)
+		L_unittest_arreq arr $'22\n' $'23\n'
+	}
+	{
+		# 4. with -t, with -n
+		local arr=()
+		L_readarray -t -s 21 -n 2 arr < <(_L_printer)
+		L_unittest_arreq arr 22 23
+	}
+}
+
+_L_test_readarray_tail_1() {
+	local L_HAS_MAPFILE=0
+	_L_test_readarray_tail_0
+}
+
+_L_test_readarray_extra_0() {
+	# Test custom FD -u
+	{
+		local arr=()
+		L_readarray -t -u 3 arr 3< <(printf "fdline1\nfdline2\n")
+		L_unittest_arreq arr fdline1 fdline2
+	}
+	# Test clearing of sparse arrays
+	{
+		local arr=()
+		arr[10]=ten
+		arr[20]=twenty
+		L_readarray -t arr < <(printf "new")
+		L_unittest_arreq arr "new"
+		L_unittest_eq "${#arr[@]}" 1
+	}
+	# Test non-newline delimiter with -t
+	{
+		local arr=()
+		L_readarray -t -d ":" arr < <(printf "x:y:z:")
+		L_unittest_arreq arr x y z
+	}
+	# Test non-newline delimiter without -t
+	{
+		local arr=()
+		L_readarray -d ":" arr < <(printf "x:y:z:")
+		L_unittest_arreq arr "x:" "y:" "z:"
+	}
+	# Test non-newline delimiter with -s and -n
+	{
+		local arr=()
+		L_readarray -t -d ":" -s 1 -n 2 arr < <(printf "a:b:c:d:e:")
+		L_unittest_arreq arr b c
+	}
+}
+
+_L_test_readarray_extra_1() {
+	local L_HAS_MAPFILE=0
+	_L_test_readarray_extra_0
+}
